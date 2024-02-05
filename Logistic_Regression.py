a6dc240d15603b6463a4bec70ccc3acf0fe46dfc
Alpedra <musacanbacak22@gmail.com>
2024-02-05 15:12:46 +0300

    importing the dataset and performing target analysis

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -8,3 +8,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score,roc_auc_score,confusion_matrix,classification_report,plot_roc_curve
 from sklearn.model_selection import train_test_split,cross_val_score
 
+
+df=pd.read_csv("Dataset/diabetes.csv")
+
+df["Outcome"].value_counts()
+sns.countplot(x="Outcome",data=df)
+plt.show()
+
+100 * df["Outcome"].value_counts() / len(df)
+
