c0adeff0acba6b21dd004ec9606b1bfca1ff4daa
Alpedra <musacanbacak22@gmail.com>
2024-02-07 14:31:01 +0300

    Create KNN class and making imports

---
+++ KNN.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.model_selection import GridSearchCV, cross_validate
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+
