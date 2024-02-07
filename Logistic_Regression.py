a2d26e52bce7bdf450fed7087de108e48f361c1f
Alpedra <musacanbacak22@gmail.com>
2024-02-07 13:42:22 +0300

    Training the model

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -16,6 +16,26 @@ def plot_numerical_col(dataframe, numerical_col):  #graphing function
 def target_summary_with_num(dataframe, target, numerical_col):
     print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
 
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75): #finding upper and lower limit
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
 df=pd.read_csv("Dataset/diabetes.csv") #import dataset
 
 df["Outcome"].value_counts()    #independent variable analysis
@@ -31,3 +51,13 @@ dependent_cols=[col for col in df.columns if "Outcome" not in col]
 
 for col in dependent_cols:
     target_summary_with_num(df, "Outcome", col)
+
+for col in dependent_cols:
+    print(col, check_outlier(df, col))
+
+replace_with_thresholds(df, "Insulin")
+
+for col in dependent_cols:
+    df[col] = RobustScaler().fit_transform(df[[col]]) #train the model
+
+df.head()
