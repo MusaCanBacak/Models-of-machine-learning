cadc5f90d050916f4c86f07f4d9170dc0b7b90e3
Alpedra <musacanbacak22@gmail.com>
2024-02-07 14:50:34 +0300

    Create model,import dataset,standardize,predictions,cross validate

--- KNN.py
+++ KNN.py
@@ -6,3 +6,27 @@ from sklearn.preprocessing import StandardScaler
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
 
+df=pd.read_csv("Dataset/diabetes.csv")
+
+y=df["Outcome"]
+X=df.drop(["Outcome"],axis=1)
+
+X_scaled=StandardScaler().fit_transform(X)
+X=pd.DataFrame(X_scaled,columns=X.columns)
+
+knn_model=KNeighborsClassifier().fit(X,y)
+random_user=X.sample(1)
+knn_model.predict(random_user)
+
+y_pred=knn_model.predict(X)
+y_prob=knn_model.predict_proba(X)[:,1]
+
+print(classification_report(y,y_pred))
+roc_auc_score(y,y_prob)
+
+cv=cross_validate(knn_model,X,y,cv=10,scoring=["f1","roc_auc","accuracy"])
+
+cv["test_f1"].mean()
+cv["test_accuracy"].mean()
+cv["test_roc_auc"].mean()
+
