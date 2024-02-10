8febb951a9d0ea98f40ca8ad366e6d8779353698
Alpedra <musacanbacak22@gmail.com>
2024-02-10 11:33:31 +0300

    Visualizing and save the Decision Tree

--- CART.py
+++ CART.py
@@ -16,30 +16,31 @@ import graphviz
 # pip install astor
 # pip install joblib
 # pip install garphviz
-
+# conda install graphviz
 
 df = pd.read_csv("Dataset/diabetes.csv")
 
 y = df["Outcome"]
 X = df.drop(["Outcome"],axis=1)
 
-cart_model=DecisionTreeClassifier().fit(X,y)
+cart_model = DecisionTreeClassifier().fit(X,y)
 
-y_pred=cart_model.predict(X)
-y_prob=cart_model.predict_proba(X)[:,1]
+y_pred = cart_model.predict(X)
+y_prob = cart_model.predict_proba(X)[:, 1]
 
-print(classification_report(y,y_pred))
+print(classification_report(y, y_pred))
 
-roc_auc_score(y,y_prob)
+roc_auc_score(y, y_prob)
 
-X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=0.20)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
 cart_model = DecisionTreeClassifier().fit(X_train ,y_train)
 
 y_pred = cart_model.predict(X_train)
-y_prob = cart_model.predict_proba(X_train)[:,1]
-print(classification_report(y_test,y_pred))
-roc_auc_score(y_test,y_prob)
+y_prob = cart_model.predict_proba(X_train)[:, 1]
+print(classification_report(y_train,y_pred))
+roc_auc_score(y_train, y_prob)
 
+# Cross validation
 cart_model = DecisionTreeClassifier().fit(X,y)
 cv_result = cross_validate(cart_model, X, y, cv=10, scoring=["accuracy","f1","roc_auc"])
 
@@ -50,11 +51,16 @@ cv_result["test_roc_auc"].mean
 #Hyperparameter optimization
 cart_model.get_params()
 
-cart_params = {'max_depth':range(1,15),
-               'min_sample_split':range(2,20)}
-cart_best_grid = GridSearchCV(cart_model,cart_params, cv=10, n_jobs=-1, verbose=1)
+cart_params = {'max_depth': range(1, 11),
+               "min_samples_split": range(2, 20)}
 
+cart_best_grid = GridSearchCV(cart_model,
+                              cart_params,
+                              cv=10,
+                              n_jobs=-1,
+                              verbose=1).fit(X, y)
 cart_best_grid.best_params_
+
 cart_best_grid.best_score_
 
 #Final model
@@ -62,13 +68,13 @@ cart_best_grid.best_score_
 cart_fnal=DecisionTreeClassifier(**cart_best_grid.best_params_).fit(X,y)
 cart_fnal.get_params()
 
-cart_fnal = cart_model.set_params(**cart_best_grid.best_params_).fit(X, y)
+cart_final = cart_model.set_params(**cart_best_grid.best_params_).fit(X, y)
 
 cv_result=cross_validate(cart_fnal, X, y, cv=10 , scoring=["accuracy","f1","roc_auc"])
 
 cv_result['test_accuracy'].mean()
 cv_result["test_f1"].mean()
-cv_result["test_roc_auc"].mena()
+cv_result["test_roc_auc"].mean()
 
 def plot_importance(model, features, num=len(X), save=False): #feature importance graph
     feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
@@ -84,3 +90,18 @@ def plot_importance(model, features, num=len(X), save=False): #feature importanc
 
 plot_importance(cart_fnal, X, num=5)
 
+# def tree_graph(model, col_name, file_name):
+#    tree_str = export_graphviz(model,feature_names=col_name,filled=True, out_file=None)
+#    graph = pydotplus.graph_from_dot_data(tree_str)
+#    graph.write_png(file_name)
+
+#tree_graph(model=cart_final, col_name=X.columns, file_name="Cart_final.png")
+#cart_final.get_params()
+
+tree_rules = export_text(cart_final, feature_names=list(X.columns))
+print(tree_rules)
+
+#saveing the model
+joblib.dump(cart_final,"cart_final.pkl")
+# loading the model
+cart_model_from_disc = joblib.load("cart_final.pkl")
