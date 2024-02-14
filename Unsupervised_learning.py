18da047fe43083f8cb28c9fb8493313ce6f267bb
Alpedra <musacanbacak22@gmail.com>
2024-02-14 19:56:33 +0300

    Making imports and getting the dataset

---
+++ Unsupervised_learning.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import random
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+from yellowbrick.cluster import KElbowVisualizer
+from scipy.cluster.hierarchy import linkage
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.preprocessing import LabelEncoder
+
+df =pd.read_csv("/Dataset/USArrests.csv")
